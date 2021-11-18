45c995ffa42c28f31238c7ddb89a51e5aeda7206
KieranVaudaux <91135493+KieranVaudaux@users.noreply.github.com>
2021-11-18 19:10:26 +0100

    continued monthly analysis

--- notebooks/streamlit_monthly.py
+++ notebooks/streamlit_monthly.py
@@ -311,9 +311,176 @@ def monthly_analysis():
     
     st.image("/Users/kieranvaudaux/Documents/SCV/SCV_project2.0/notebooks/figure/Monthly/Seasonless_mean_acf_pacf.png")
     
-    st.markdown(r"")
+    with st.expander("Explanation of the test for the acf and pacf plot"):
+        st.markdown(r'On the two plot above, the blue zone corresponds to an approximate confidence interval for the acf and pacf under the null hypothesis that the acf and pacf are computedf from a iid sequence. This test is based on the fact that for large n the sample autocorrelations of an iid sequence $Y_1, . . . , Y_n$ with finite variance are approximately iid with distribution $\mathbb{N}(0, \frac{1}{n})$. We can therefore test whether or not the observed residuals are consistent with iid noise by examining the sample autocorrelations and partial autocorrelations of the residuals and rejecting the iid noise hypothesis if more than three or four out of 50 fall outside the bounds $\pm \frac{1.96}{\sqrt{n}}$ or if one falls far outside the bounds.')
+    
+    st.markdown(r"On these two plots, we see that there is a lot of points outside the blue zone, which is a indication to the presence of dependency between the elements of the time series. In order to confirm this suspicion of dependence between elements, we have on the following figure the p-values of the Ljung-Box and McLeod-Li tests for different sizes of auto-correlation sequences.")
+    
+    pval_ind1 = pd.read_csv("DataGenerated/Monthly/Monthly1_pvalue_indep_GLS")
+    
+    fig = make_subplots(
+        rows=2, cols=3,
+        row_heights=[1,1],
+        column_widths=[1./10,8./10,1./10],
+        specs=[[None,{"type": "Scatter"},None],[None,{"type": "Scatter"},None]],
+        x_title = 'Autocorrelation lags'
+        )
+        
+    fig.add_trace(go.Scatter(x=pval_ind1.index+1, y=pval_ind1.Ljung,
+        mode = 'lines+markers', name = 'Ljung-Box'),row =1, col = 2)
+    fig.add_trace(go.Scatter(x=pval_ind1.index+1, y=pval_ind1.McLeod,
+        mode = 'lines+markers',name = 'McLeod-Li'),row =2, col = 2)
+    fig.add_hline(y=0.05,line_dash="dash", line_color="red", name = 'alpha = 0.05')
+    
+    fig['layout'].update({
+        'title': 'P-values of the Ljung-Box and McLeod-Li tests',
+        'title_x': 0.5,
+        'xaxis': {
+            'zeroline': False
+        },
+        'yaxis': {
+            'type': 'log'
+        },
+        'width': 1200,
+        'height': 500,
+    })
+    fig.update_yaxes(title_text="p-value", col=2)
+    
+    st.plotly_chart(fig)
+    
+    with st.expander("Explanation of the Ljung-Box and McLeod-Li tests"):
+        st.markdown(r'''
+        The idea behind these two tests is, instead of checking to see whether each sample autocorrelation $\rho (j)$ falls inside the bounds $\pm \frac{1.96}{\sqrt{n}}$, it is also possible to consider the either the statistic $\mathbf{Q}_{LB} = n(n+2)\sum_{j=1}^{h} \frac{\rho^2 (j)}{n-j}$ for the Ljung-Boy tests or the statistic $\mathbf{Q}_{ML} = n(n+2)\sum_{j=1}^{h} \frac{\rho_{ww}^2 (j)}{n-j}$ for the McLeod-Li test, where $\rho (j)$ is the acf and $\rho_{ww}(j)$ if the acf the squared data. Then, under the null hypothesis that our data are iid, this both statistics could be approximated by a chi-squared distribution with $h$ degree of freedom.
+        ''')
+        
+    st.markdown(r"Although many of the p-values in the McLeod-Li test do not allow us to reject the hypothesis of independence, we have that the p-values in the Ljung-Box test are all well below the significance level of $\alpha = 0.05$, so they allow us to reject the null hypothesis of independence. Then, we now have choose a appropriate time series model to model our time series $\{\mathbf{\tilde{M}}_{t,m_t}\}$. The following figure allows us to compare several models for the time series thanks to the Akaike information criterion (AIC). We use the AIC in order to make a model selection since it is an estimator of prediction error and thereby relative quality of statistical models for our set of data. We have tried to model the time series as arising from an $ARMA(p,q)$ for $p\in\{0,...,4\}$ and $q\in\{0,...,11\}$. Our choice of restricting $p$ and $q$ is mainly due to the fact that we wanted to try to keep the model as simple as possible to model our data. To estimate the coefficients of a given model, we use the method 'ARIMA' in the \textbf{statsmodels} packages on python.")
+    
+    aic1 = pd.read_csv("DataGenerated/Monthly1_modelSelection.csv")
+    
+    fig = make_subplots(
+        rows=1, cols=3,
+        row_heights=[1],
+        column_widths=[1./10,8./10,1./10],
+        specs=[[None,{"type": "Scatter"},None]],
+        x_title = 'AR(p) parameters'
+        )
+    p = np.arange(0,12)
+    for i in range(5):
+        fig.add_trace(go.Scatter(x=p, y=aic1[aic1.param_q==i].AIC,
+            mode = 'lines+markers', name = 'q = '+str(i),
+            marker=dict(size=12,color=px.colors.qualitative.Dark24[i])),row =1, col = 2)
+    
+    #fig.add_hline(y=0.05,line_dash="dash", line_color="red", name = 'alpha = 0.05')
+    
+    fig['layout'].update({
+        'title': 'Time series model selection for the seasonless mean temperature',
+        'title_x': 0.5,
+        'xaxis': {
+            'zeroline': False
+        },
+        'yaxis': {
+            'type': 'log'
+        },
+        'width': 1200,
+        'height': 500,
+    })
+    fig.update_yaxes(title_text="AIC", row=1, col=2)
+    fig.update_layout(hovermode="x unified")
+    
+    #fig.update_layout(legend=dict(y=0.99,x=0.86))
+    
+    st.plotly_chart(fig)
+
+    st.markdown(r'By choosing the model which minimises the AIC, we are led to consider the model $ARMA(4,3)$ to model the time series $\{\mathbf{\tilde{M}}_{t,m_t}\}$. On the following plot, we see the residus of the estimation of the trendless time series as a $ARMA(4,3)$ process, where the "ARIMA" method estimate the coefficient with $\hat{\phi}_1 = -0.039238, \hat{\phi}_2 = 0.310470 , \hat{\phi}_3 = 0.893256, \hat{\phi}_4 = -0.180764, \hat{\theta}_1 = 0.225011, \hat{\theta}_2 = -0.210028, \hat{\theta}_3 = -0.917060 \text{ and } \hat{\sigma}^2 = 2.332686$ where the $\hat{\phi}_i$ are the autoregressive coefficients, the $\hat{\theta}_i$ are the moving average coefficients.')
+    
+    resid_arma4_3 =pd.read_csv("DataGenerated/Monthly/Monthly_arma4_3.csv")
+        
+    fig = px.line(resid_arma4_3, x='Date', y='resid')
+
+    fig.update_xaxes(
+        rangeslider_visible=True,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=6, label="6 months", step="month", stepmode="backward"),
+                dict(count=1, label="1 year", step="year", stepmode="backward"),
+                dict(count=10, label="10 years", step="year", stepmode="backward"),
+                dict(count=30, label="30 years", step="year", stepmode="backward"),
+                dict(count=50, label="50 years", step="year", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+    
+    fig['layout'].update({
+        'title': 'Residus of the ARMA(4,3) model',
+        'title_x': 0.5,
+        'xaxis': {
+            'zeroline': False
+        },
+        'yaxis': {
+            'title':"Residus mean temperature(°C)"
+        },
+        
+        'width': 1200,
+        'height': 500,
+        'legend':{
+            'title':""
+                         }
+    })
+   
+    st.plotly_chart(fig)
+    
+    st.markdown(r'In order to confirm the consistency of our model, we tested the independence and the distribution of the residuals obtained. The following figure show us the result of the Ljung-Box and McLeod-Li test on the residuals of our ARMA(4,3) model, we see that all p-values are far above the significance threshold of $\alpha = 0.05$, and thus do not allow us to reject the null hypothesis of independence of the tests. Therefore, the $ARMA(4,3)$ model seems to capture well the correlation structure between the elements of the time series $\{\mathbf{\tilde{M}}_{t,m_t}\}$.')
     
     
+    pval_ind2 = pd.read_csv("DataGenerated/Monthly/Monthly2_pvalue_indep_GLS")
     
+    fig = make_subplots(
+        rows=1, cols=3,
+        row_heights=[1],
+        column_widths=[1./10,8./10,1./10],
+        specs=[[None,{"type": "Scatter"},None]],
+        x_title = 'Autocorrelation lags'
+        )
+        
+    fig.add_trace(go.Scatter(x=pval_ind2.index+1, y=pval_ind2.Ljung,
+        mode = 'lines+markers', name = 'Ljung-Box'),row =1, col = 2)
+    fig.add_trace(go.Scatter(x=pval_ind2.index+1, y=pval_ind2.McLeod,
+        mode = 'lines+markers',name = 'McLeod-Li'),row =1, col = 2)
+    #fig.add_hline(y=0.05,line_dash="dash", line_color="red", name = 'alpha = 0.05')
     
+    fig['layout'].update({
+        'title': 'P-values of the Ljung-Box and McLeod-Li tests on the residus of the ARMA(4,3)',
+        'title_x': 0.5,
+        'xaxis': {
+            'zeroline': False
+        },
+        'yaxis': {
+        },
+        'width': 1200,
+        'height': 500,
+    })
+    fig.update_yaxes(title_text="p-value")
     
+    st.plotly_chart(fig)
+    
+    st.markdown(r"Moreover,as we could expect the residues are of the ARMA(4,3) model are stationary since we obtain a p-value of $p = 0$ for the Augmented Dickey-Fuller test. Thus, we also could check the plots of the acf anf pacf, on the followings figures, to asses the remaining correlation structure in the residus. And we could see that all the autocorrelation lies in the blue 95% confidence interval, which confirms that it is reasonable to assume that the residuals are independent. ")
+    
+    st.image("/Users/kieranvaudaux/Documents/SCV/SCV_project2.0/notebooks/figure/Monthly/Seasonless_mean_acf_pacf.png")
+    
+    st.markdown(r'Nevertheless, after performing the Jarque-Bera test, which has the null hypothesis that our data are from a normal distribution, and the Goldfeld-Quandt test, which has the null hypothesis that the data are homoskedastic, we get the p-values : ')
+
+    col1,col2,col3 = st.columns([1,1,1])
+    
+    with col2:
+        st.latex(r'''\text{Normality test : }p_{value} = 8.411e-15 \\
+                \text{Heteroskedasticity test : }p_{value} = 0.787
+        ''')
+        
+    with st.expander("Jarque-Bera and Goldfeld_Quandt tests"):
+        st.markdown(r'''$\textbf{The Jarques-Bera test}$ :  is a goodness-of-fit test of whether sample data have the skewness and kurtosis matching a normal distribution. The test statistic is defined as : $JB = \frac{n}{6} \left( S^2 + \frac{1}{4} \left(K-3\right)^2 \right)$, where $S =\frac{ \frac{1}{n} \sum_{i=1}^{n} (x_i-\bar{x})^3 }{ \left( \frac{1}{n} \sum_{i=1}^n (x_i - \bar{x})^2 \right)^{\frac{3}{2}} }$ and $K =\frac{ \frac{1}{n} \sum_{i=1}^{n} (x_i-\bar{x})^4 }{ \left( \frac{1}{n} \sum_{i=1}^n (x_i - \bar{x})^2 \right)^{2} }$.\\
+            If the data comes from a normal distribution, the JB statistic asymptotically has a chi-squared distribution with two degrees of freedom, so the statistic can be used to test the hypothesis that the data are from a normal distribution. The null hypothesis is a joint hypothesis of the skewness being zero and the excess kurtosis being zero.''')
+        st.markdown(r'''$\textbf{The Goldfeld-Quandt tests}$ : is accomplished by undertaking separate least squares analyses on two subsets of the original dataset: these subsets are specified so that the observations for which the pre-identified explanatory variable takes the lowest values are in one subset, with higher values in the other. The subsets needs not be of equal size, nor contain all the observations between them. The test assumes that the errors have a normal distribution. There is an additional assumption here, that the design matrices for the two subsets of data are both of full rank. The test statistic used is the ratio of the mean square residual errors for the regressions on the two subsets. This test statistic corresponds to an F-test of equality of variances.
+            ''')
+    
+    st.markdown(r"Even if the p-value of the test is significant at a threshold of $\alpha = 0.05$, we have that the normality test is widely rejected. Thus, this leads us to believe that the model we have developed so far may not be appropriate for our data. ")
